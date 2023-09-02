be3d3dd652c653ab0901fa741a7609bdbb545564
Shaikat roy <shaikatroy258@gmail.com>
2023-09-03 01:16:39 +0600

    footer updated

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,48 +1,93 @@
+import React from "react";
+import { FaFacebook, FaTwitter, FaInstagram, FaMapMarkerAlt, FaEnvelope, FaPhone } from "react-icons/fa";
+import Link from "next/link";
 
-const Footer = () => {
-    return (
-        <footer className="bg-gradient-to-r from-green-400 via-blue-500 to-purple-600 text-white py-10">
-            <div className="container mx-auto grid grid-cols-4 gap-8">
-               
-                <div>
-                    <h1 className="text-2xl font-bold mb-4">Fun Trek</h1>
-                    <p>Experience the best summer with us! Join Camp Sunshine for unforgettable memories and adventures.</p>
-                </div>
+const Footer: React.FC = () => {
+  const socialLinks = [
+    { name: "Facebook", url: "https://www.facebook.com/yourcamp", icon: <FaFacebook /> },
+    { name: "Twitter", url: "https://twitter.com/yourcamp", icon: <FaTwitter /> },
+    { name: "Instagram", url: "https://www.instagram.com/yourcamp", icon: <FaInstagram /> },
+  ];
 
-            
-                <div>
-                    <h3 className="text-xl font-bold mb-4">Activities</h3>
-                    <ul>
-                        <li className="mb-2 transform transition-transform hover:translate-x-2">Hiking</li>
-                        <li className="mb-2 transform transition-transform hover:translate-x-2">Arts & Crafts</li>
-                        <li className="mb-2 transform transition-transform hover:translate-x-2">Swimming</li>
-                    </ul>
-                </div>
-
-            
-                <div>
-                    <h3 className="text-xl font-bold mb-4">Contact</h3>
-                    <ul>
-                        <li className="mb-2">Phone: (123) 456-7890</li>
-                        <li className="mb-2">Email: info@campsunshine.com</li>
-                        <li className="mb-2">Address: 123 Camp St, Sunshine City</li>
-                    </ul>
-                </div>
-
-                <div>
-                    <h3 className="text-xl font-bold mb-4">Registration</h3>
-                    <ul>
-                        <li className="mb-2 transform transition-transform hover:translate-x-2">Pricing</li>
-                        <li className="mb-2 transform transition-transform hover:translate-x-2">Sign Up</li>
-                        <li className="mb-2 transform transition-transform hover:translate-x-2">Testimonials</li>
-                    </ul>
-                </div>
+  return (
+    <footer className="text-primary font-medium relative px-2 bg-gradient-to-r from-black via-gray-800 to-black">
+      <div className="container mx-auto py-8">
+        <div className="grid grid-cols-1 lg:grid-cols-4 gap-8">
+            <div className="text-center lg:text-left">
+              <h1 className="text-3xl font-bold mb-4 text-white">Fun Trek</h1>
+              <p className="mt-4 text-white">
+                Join Camp Sunshine for an unforgettable summer filled with adventures and friendships.
+              </p>
+              <div className="mt-4 flex items-center">
+                {socialLinks.map((link) => (
+                  <a
+                    key={link.name}
+                    href={link.url}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="hover:text-secondary text-2xl mr-4 transition-colors duration-300"
+                  >
+                    {link.icon}
+                  </a>
+                ))}
+              </div>
             </div>
-            <div className="mt-10 border-t border-gray-300 pt-4 text-center">
-                &copy; {new Date().getFullYear()} Camp Sunshine. All rights reserved.
+            <div>
+              <h3 className="text-lg font-bold mb-4 text-white">Activities</h3>
+              <ul>
+                <li className="mb-2 text-white transition-all duration-300 hover:text-secondary hover:ml-4">
+                  <Link href="/hiking">Hiking</Link>
+                </li>
+                <li className="mb-2 text-white transition-all duration-300 hover:text-secondary hover:ml-4">
+                  <Link href="/arts-crafts">Arts & Crafts</Link>
+                </li>
+                <li className="mb-2 text-white transition-all duration-300 hover:text-secondary hover:ml-4">
+                  <Link href="/swimming">Swimming</Link>
+                </li>
+              </ul>
             </div>
-        </footer>
-    );
+            <div>
+              <h3 className="text-lg font-bold mb-4 text-white">For Campers</h3>
+              <ul>
+                <li className="mb-2 text-white transition-all duration-300 hover:text-secondary hover:ml-4">
+                  <Link href="/schedule">Schedule</Link>
+                </li>
+                <li className="mb-2 text-white transition-all duration-300 hover:text-secondary hover:ml-4">
+                  <Link href="/login">Login</Link>
+                </li>
+                <li className="mb-2 text-white transition-all duration-300 hover:text-secondary hover:ml-4">
+                  <Link href="/register">Register</Link>
+                </li>
+                <li className="mb-2 text-white transition-all duration-300 hover:text-secondary hover:ml-4">
+                  <Link href="/dashboard">Camper Dashboard</Link>
+                </li>
+              </ul>
+            </div>
+            <div>
+              <h3 className="text-lg font-bold mb-4 text-white">Contact Us</h3>
+              <ul>
+                <li className="mb-2 flex items-center text-white">
+                  <FaMapMarkerAlt className="mr-2 hover:text-secondary transition-colors duration-300" />
+                  123 Camp Street, Sunshine City, Country
+                </li>
+                <li className="mb-2 flex items-center text-white">
+                  <FaEnvelope className="mr-2 hover:text-secondary transition-colors duration-300" />
+                  Email: info@campsunshine.com
+                </li>
+                <li className="mb-2 flex items-center text-white">
+                  <FaPhone className="mr-2 hover:text-secondary transition-colors duration-300" />
+                  Phone: +123 456 7890
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="mt-5 text-center">
+            <hr className="border-white"/>
+            <p className="text-white mt-3">&copy; {new Date().getFullYear()} Fun Trek. All rights reserved.</p>
+          </div>
+        </div>
+    </footer>
+  );
 };
 
 export default Footer;
